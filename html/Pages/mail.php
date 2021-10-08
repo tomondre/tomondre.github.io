40958abc2911b0dd951582d6e07150add66b3004
tomondre <304150@via.dk>
2021-10-08 19:44:40 +0200

    Updating counter widgets on websites + adding email handler

--- html/Pages/mail.php
+++ html/Pages/mail.php
@@ -5,10 +5,9 @@ if(isset($_POST['submit']))
     $email = $_POST['email'];
     $text = $_POST['text'];
 
-    $mailTo = "tomas.ondrejka2000@gmail.com";
+    $mailTo = "304150@via.dk";
     $headers = "From:".$email;
     $txt = "You have received email from".$name.".\n\n".$text;
-
 
     mail($mailTo,$name,$txt);
 }
